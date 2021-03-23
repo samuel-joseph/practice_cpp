0082f1a71d6a79bbb4e19d81dd6caa79f62eab7b
arcanlte <pinangayjoel@gmail.com>
2021-03-23 19:20:42 -0400

    int, char, string, double

---
+++ variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+  int myInt, myInt1, myInt2;
+  double myDouble = 3.14;
+  char singleChar = 'a';
+  string myName = "Sam";
+  myInt = 15;
+  myInt1 = 2;
+  myInt2 = myInt + myInt1;
+
+  cout << myInt2 << endl;
+  cout << myDouble << endl;
+  cout << myName << endl;
+  return 0;
+}
