7e92941bc9f4acaf3b0b72947c99f0bbbb30d7f6
arcanlte <pinangayjoel@gmail.com>
2021-03-23 21:41:01 -0400

    File initiated

---
+++ RelationalFun.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+/*
+    >  greater than
+    >= greather than or equal to
+    < less than
+    < less than or equal to
+    == equal-to
+    != not-equal-to
+*/
+
+int main()
+{
+
+  cout << boolalpha;
+
+  int a = 15;
+  int b = 20;
+
+  cout << (a < b) << endl;
+
+  return 0;
+}
