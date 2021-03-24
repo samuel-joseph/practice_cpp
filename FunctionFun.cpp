2abf274361a625f6e15debe8dad5492a6446743e
arcanlte <pinangayjoel@gmail.com>
2021-03-24 13:33:05 -0400

    Function.cpp is learned

---
+++ FunctionFun.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void printSomething();
+int main()
+{
+
+  printSomething();
+
+  return 0;
+}
+
+void printSomething()
+{
+  cout << "This is a function" << endl;
+}
